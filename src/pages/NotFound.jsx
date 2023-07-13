7954cfb0d9584a98c7dfeda5e384744e2d1fd97c
Dev Kaliper <gxlthz@gmail.com>
2023-07-12 20:51:58 -0500

    not found agregado

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
 
 const NotFound = () => {
     const error = useRouteError();
@@ -7,6 +7,8 @@ const NotFound = () => {
     return (
         <div>
             <h1>404</h1>
+            <p>Lo sentimos, la pagina a la que intentas acceder no existe.</p>
+            <button><Link to="/"></Link>Volver al origen </button>
             <p>Page not found</p>
             <p>{error.statusText || error.message}</p>
         </div>
