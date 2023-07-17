bdeeb1d64cf437454cbf1d9a346ff5b864bf3a34
Dev Kaliper <gxlthz@gmail.com>
2023-07-16 21:30:52 -0500

    not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,9 +7,9 @@ const NotFound = () => {
     return (
         <div>
             <h1>404</h1>
-            <p>Lo sentimos, la pagina a la que intentas acceder no existe.</p>
+            <p className="text-center">Lo sentimos, la pagina a la que intentas acceder no existe.</p>
             <button><Link to="/"></Link>Volver al origen </button>
-            <p>Page not found</p>
+            <p className="text-center">Page not found</p>
             <p>{error.statusText || error.message}</p>
         </div>
     );
