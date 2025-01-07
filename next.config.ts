d702d9d78cb25c0bac1bd47694f96895f13e5c29
DevKaliper <gxlthz@gmail.com>
2025-01-07 10:20:50 -0500

    start over

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+  basePath: "/Encripte",
+
 };
 
 export default nextConfig;
